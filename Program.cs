18e58efccd592567ff95becbb0c8a1572049cd3e
Yasser <43596987+YasserAlShafei@users.noreply.github.com>
2022-04-19 18:16:56 +0300

    IndependentExperience

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace democonsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Enter radius : ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter length : ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            double area = radius * radius * 3.14;
+            double volume = area * length;
+
+            Console.WriteLine("=> Value Area is = " +  radius + " * " + radius + " * " + 3.14 + " = " + area);
+            Console.WriteLine("=> Value volume is = " +  area + " * " + length + " = " + volume);
+        }
+    }
+}
